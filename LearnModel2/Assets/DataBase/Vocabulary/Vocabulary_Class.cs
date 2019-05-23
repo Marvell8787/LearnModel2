f6cb4cecdb363ad72acebb5c4b092dd0e2fea360
Marvell8787 <wendy850122@gmail.com>
2019-05-23 20:05:24 +0800

    20190523
    Material Interface 80%

---
+++ LearnModel2/Assets/DataBase/Vocabulary/Vocabulary_Class.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Vocabulary_Class{
+    private string E_Name = "";
+    private string C_Name = "";
+    private string Voice = "";
+}
---
+++ LearnModel2/Assets/Script/Learn/Fuction_Learn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuction_Learn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void GoMaterial()
+    {
+        Application.LoadLevel("Material");
+    }
+    public void GoLevel()
+    {
+        Application.LoadLevel("Level");
+    }
+    public void BackHome()
+    {
+
+    }
+	
+}
