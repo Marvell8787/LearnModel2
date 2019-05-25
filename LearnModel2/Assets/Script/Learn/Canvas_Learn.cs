953c8ef77b6f0ad51a8ad9156cecf5b5edb3c849
Marvell8787 <wendy850122@gmail.com>
2019-05-26 02:24:51 +0800

    20190526

--- LearnModel2/Assets/Script/Learn/Canvas_Learn.cs
+++ LearnModel2/Assets/Script/Learn/Canvas_Learn.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Canvas_Learn : MonoBehaviour {
+     Level_Class[] level_temp = new Level_Class[7];
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        Text t_temp;
+
+        Level_Data.Level_Init();
+        for (int i = 0; i < 6; i++)
+        {
+            level_temp[i] = Level_Data.Level_Get(i);
+            t_temp = GameObject.Find("Text_Level-" + (i+1).ToString() + "_Score").GetComponent<Text>();
+            t_temp.text = level_temp[i].GetHighestScore();
+        }
+        level_temp[6] = Level_Data.Level_Get(6);
+        t_temp = GameObject.Find("Text_Overall_Score").GetComponent<Text>();
+        t_temp.text = level_temp[6].GetHighestScore();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
