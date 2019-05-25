c71a7f2a16f71b76d4c68a12389c7685874f0992
Marvell8787 <wendy850122@gmail.com>
2019-05-26 03:02:49 +0800

    20190526
    Level not compelet

---
+++ LearnModel2/Assets/DataBase/Level_Learn/Question_Class.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Question_Class{
+    private string QuestionType = "";
+    private string QuestionNum = "";
+    private string Question = "";
+    private string Answer = "";
+    private string FeedBack = "";
+    public Question_Class(string _QuestionType = "", string _QuestionNum = "", string _Question = "", string _Answer = "", string _FeedBack = "")
+    {
+        QuestionType = _QuestionType;
+        QuestionNum = _QuestionNum;
+        Question = _Question;
+        Answer = _Answer;
+        FeedBack = _FeedBack;
+    }
+    private string GetQuestionType()
+    {
+        return QuestionType;
+    }
+    private string GetQuestionNum()
+    {
+        return QuestionNum;
+    }
+    private string GetQuestion()
+    {
+        return Question;
+    }
+    private string GetAnswer()
+    {
+        return Answer;
+    }
+    private string GetFeedBack()
+    {
+        return FeedBack;
+    }
+    private void ChangeFeedBack(string s)
+    {
+        FeedBack = s;
+    }
+}
---
+++ LearnModel2/Assets/DataBase/Level_Learn/Question_Data.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+static class Question_Data{
+    public static string[] Question_QuestionType = new string[7] { "Listening", "Fill in the blanks", "Translation", "Listening", "Fill in the blanks", "Translation", "Spelling" };
+    public static string[] Question_Title = new string[7] { "Level-1", "Level-2", "Level-3", "Level-4", "Level-5", "Level-6", "Overall" };
+    public static string[] Question_Range = new string[7] { "1-5", "1-5", "1-5", "6-10", "6-10", "6-10", "6-10" };
+    public static string[] Question_Reward = new string[7] { "Money", "Money", "Money", "Money", "Money", "Money", "Money" };
+    public static string[] Question_Punishment = new string[7] { "Money", "Money", "Money", "Money", "Money", "Money", "Money" };
+    public static string[] Question_HighestScore = new string[7] { "0", "0", "0", "0", "0", "0", "0" };
+}
